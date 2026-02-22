fa9b0bb3a7f03b2c6779a5408b10617963f3a4a8
CGK <85840211+ChethanKottari@users.noreply.github.com>
2026-02-22 19:56:08 +0530

    initial commit

---
+++ transactionroutine/src/main/java/com/pismo/transactionroutine/TransactionroutineApplication.java
@@ -0,0 +1,13 @@
+package com.pismo.transactionroutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionroutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionroutineApplication.class, args);
+	}
+
+}
---
+++ transactionroutine/src/test/java/com/pismo/transactionroutine/TransactionroutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pismo.transactionroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionroutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
